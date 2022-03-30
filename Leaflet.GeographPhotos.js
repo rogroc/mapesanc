21de53af535e0ac283312829813bd787306a9115
rogroc <72193758+rogroc@users.noreply.github.com>
2022-03-30 09:17:22 +0200

    Update Leaflet.GeographPhotos.js

--- Leaflet.GeographPhotos.js
+++ Leaflet.GeographPhotos.js
@@ -201,7 +201,7 @@ if (L.MarkerClusterGroup && L.Photo.Cluster) {
 
 			var that = this; //enclosure!
 			this._request = reqwest({
-				url: 'data_2.json',
+				url: 'https://gencat-my.sharepoint.com/personal/rogerrocavert_gencat_cat/_layouts/15/download.aspx?UniqueId=8167f5f768464082ab3e250739fbaf90&e=2R8g6F',
 				type: 'jsonp',
 				jsonpCallbackName: 'bar', //we use a specific callback name, so it has consistent name, and hence the server can cache this API call. 
 				success: function (data) {
@@ -288,7 +288,7 @@ document.write('arr2', arr2);
 				}
 				var that = this; //enclosure!
 				this._request =  reqwest({
-	                		url: 'data_2.json',
+	                		url: 'https://gencat-my.sharepoint.com/personal/rogerrocavert_gencat_cat/_layouts/15/download.aspx?UniqueId=8167f5f768464082ab3e250739fbaf90&e=2R8g6F',
 			                type: 'jsonp',
 			                success: function (data) {
 						that._running = false;
